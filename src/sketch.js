a941c98a645cfec03d6913fe34a20c85f7371268
Gururaj Shriram <gururajshriram@gmail.com>
2018-04-06 17:50:54 -0400

    initial commit

---
+++ src/sketch.js
@@ -0,0 +1,14 @@
+/*
+  CIM540/CIM542 Final Project
+
+  authors: jerry bonnell and gururaj shriram
+  date last modified: 6 apr 2018
+*/
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+}
+
+function draw() {
+
+}
